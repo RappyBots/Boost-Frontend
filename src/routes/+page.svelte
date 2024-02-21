a63e78b040f1e9b10a45b86b7f53c6d802cd1a99
holybaechu <joonhoo0524@naver.com>
2024-02-21 16:25:14 +0900

    Responsive design for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,20 +7,20 @@
 	import Logo from "$lib/images/logo.webp"
 </script>
 
-<div class="min-h-[720px] max-w-5xl pt-48 text-center space-y-12 mx-auto">
-	<div class="text-5xl font-bold bg-gradient-to-b from-white to-gray-600 bg-clip-text text-transparent">
+<div class="min-h-screen max-w-5xl pt-32 md:pt-64 text-center space-y-12 mx-10 lg:mx-auto">
+	<div class="text-4xl md:text-5xl font-bold bg-gradient-to-b from-white to-gray-600 bg-clip-text text-transparent">
 		The best way to get notified when your members boost your server
 	</div>
-	<div class="text-2xl text-gray-500">
+	<div class="text-xl md:text-2xl text-gray-500">
 		Get notified when someone boosts your server with <strong class="text-white">flawless customization powered by Discord embeds</strong>, and reward them with a custom role
 	</div>
 
-	<div class="flex justify-center space-x-3">
+	<div class="flex flex-col sm:flex-row justify-center space-y-3 sm:space-y-0 sm:space-x-3">
 		<!-- Invite Btn -->
 		<Button className="bg-primary hover:bg-accent" href="/invite">
-			<div class="flex h-full">Invite<img src={Logo} class="h-[24px] object-contain" alt="Boost Notifications Logo"/>Boost Notifications</div>
+			<div class="flex h-full justify-center sm:justify-start">Invite<img src={Logo} class="h-[24px] object-contain" alt="Boost Notifications Logo"/>Boost Notifications</div>
 		</Button>
-
+	
 		<!-- Support Btn -->
 		<Button className="bg-transparent outline outline-1 outline-gray-600 hover:bg-gray-700" href="/support">
 			Join support server
--- src/routes/invite/+page.svelte
+++ src/routes/invite/+page.svelte
@@ -1,1 +1,5 @@
 <!-- Empty as this page will processed by Ngnix and because of SSR, If I remove this page, Error will appear while building. -->
+
+<div class="flex text-center justify-center text-4xl pt-[50vh]">
+    Redirecting...
+</div>
