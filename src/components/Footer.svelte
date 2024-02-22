5f53991354de81a448224e0c664d9a737b99b483
RappyTV <rappytv@rappytv.com>
2024-02-22 16:01:05 +0100

    Dynamic footer year

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
     <div class="flex flex-col p-10 h-full space-y-16 text-gray-400">
         <a href="/contact">
             <div class="text-2xl font-semibold">RappyTV</div>
-            <div class="text-md">© 2024 RappyTV</div>
+            <div class="text-md">© {new Date().getFullYear()} RappyTV</div>
         </a>
 
         <a href="https://holyb.xyz/">Frontend by holybaechu.</a>
