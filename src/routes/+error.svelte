a6ab83b123c1f3b96a9138b05d3c0ce9e9c31701
holybaechu <holybaechu@github.com>
2024-02-23 02:09:14 +0900

    Create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex flex-col h-screen justify-center items-center space-y-1">
+    <div class="text-6xl font-black">
+        {$page.status}
+    </div>
+    <div class="text-3xl">{$page.error.message}</div>
+</div>
