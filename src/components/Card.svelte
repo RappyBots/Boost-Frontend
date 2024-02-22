0ab18bb37cc0e61382684a222a3c24c9ec582e82
holybaechu <joonhoo0524@naver.com>
2024-02-22 20:57:40 +0900

    Add cards

---
+++ src/components/Card.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let title = "";
+</script>
+
+<div class="h-80 bg-[#ffffff04] rounded-lg outline outline-1 outline-[#cbd5e11a] p-6 space-y-2">
+    <div class="text-2xl font-semibold">{title}</div>
+    <div class="text-md text-gray-400">
+        <slot/>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,12 @@
 
 <script>
 	import Button from '$components/Button.svelte';
+	import Card from '$components/Card.svelte';
+
 	import Logo from "$lib/images/logo.webp"
 </script>
 
-<div class="min-h-[880px] max-w-5xl pt-32 md:pt-64 text-center space-y-12 mx-10 lg:mx-auto">
+<div class="min-h-[820px] max-w-5xl pt-32 md:pt-64 text-center space-y-12 mx-10 lg:mx-auto">
 	<div class="text-4xl md:text-5xl font-black bg-gradient-to-b from-white to-gray-600 bg-clip-text text-transparent">
 		The best way to get notified when your members boost your server
 	</div>
@@ -25,5 +27,32 @@
 		<Button className="bg-transparent outline outline-1 outline-gray-600 hover:bg-gray-700" href="/support">
 			Join support server
 		</Button>
+	</div>
+</div>
+
+
+<div class="max-w-5xl mx-10 lg:mx-auto py-10">
+	<div class="text-3xl md:text-4xl font-bold text-center">Why Boost Notifications?</div>
+
+	<!-- Cards -->
+	<div class="grid gap-4 mt-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+		<Card title="🕑 Uptime">
+			We will work hard to ensure that our bot is always ready to send notifications when someone boosts your server.
+		</Card>
+		<Card title="✨ Customization">
+			You can customize the image of the embed and DM messages sent to booster to fit your server's style.
+		</Card>
+		<Card title="🌍 Multi Language">
+			You can change the language of the bot's reponses, command names and descriptions to ensure that your server members can use the bot in their own language.
+		</Card>
+		<Card title="🛠️ Easy Management">
+			You can manage which roles will be given to the booster and which channel will the notifications be sent to with just one command.
+		</Card>
+		<Card title="🚀 Boost Simulation">
+			You can simulate a boost to test if the bot is working correctly and to see how the notifications will look like.
+		</Card>
+		<Card title="📞 Fast Support">
+			We have a dedicated support server where you can ask for help, report bugs and suggest new features and we will respond as soon as possible.
+		</Card>
 	</div>
 </div>
