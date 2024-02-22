15b735bf5ff021f890e9ba5010f4e906b723df3d
holybaechu <joonhoo0524@naver.com>
2024-02-22 21:30:05 +0900

    Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<div class="flex h-56 bg-[#00000025] justify-between">
+    <div class="flex flex-col p-10 h-full space-y-16 text-gray-400">
+        <a href="https://rappytv.com/">
+            <div class="text-2xl font-semibold">RappyTV</div>
+            <div class="text-md">© 2024 RappyTV</div>
+        </a>
+
+        <a href="https://holyb.xyz/">Frontend by holybaechu.</a>
+    </div>
+
+    <!-- Links -->
+    <div class="flex flex-col p-10 text-gray-400 space-y-4 text-right">
+        <a href="/invite">
+            Bot invite
+        </a>
+        <a href="/support">
+            Support
+        </a>
+        <a href="/contact">
+            Contact
+        </a>
+    </div>
+</div>
