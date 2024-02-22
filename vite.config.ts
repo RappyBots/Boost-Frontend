46e7fd4d5915d59931f516b350b79def62a2af40
RappyTV <rappytv@rappytv.com>
2024-02-22 18:32:51 +0100

    Fix build errors because of dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
