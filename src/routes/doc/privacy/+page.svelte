246782087b91e489150cc93ee77f59db35a13d24
holybaechu <joonhoo0524@naver.com>
2024-02-23 19:37:38 +0900

    Add privacy policy page

---
+++ src/routes/doc/privacy/+page.svelte
@@ -0,0 +1,75 @@
+<div class="p-40 space-y-6">
+    <div class="text-center text-4xl font-black">
+        Privacy Policy
+    </div>
+
+    <div class="flex flex-col space-y-5">
+        <div>
+            This Privacy Policy ("Policy") outlines how we collect, use, store, and protect the information obtained from users who utilize our Discord bot designed to provide notifications when someone boosts a server. We are committed to safeguarding your privacy and ensuring the security of your personal data. By using our bot, you consent to the practices described in this Policy.
+        </div>
+        
+       <div class="space-y-2">
+            <div class="text-3xl font-bold">1. Information Collection</div>
+            <div class="space-y-2">
+                <div>
+                    a. User Data: When you interact with our bot, we may collect certain data to provide the server boost notifications. This data may include your Discord user ID, server ID, and boost timestamp. We do not collect or store any additional personal information.
+                </div>
+                <div>
+                    b. Aggregated Data: We may collect aggregated, non-personally identifiable information for statistical purposes, such as the total number of server boosts recorded or the frequency of boosts across servers. This information does not identify individual users.
+                </div>   
+            </div>
+       </div> 
+
+       <div class="space-y-2">
+            <div class="text-3xl font-bold">2. Use of Information</div>
+            <div class="space-y-2">
+                <div>
+                    a. Service Provision: We use the collected data to deliver the server boost notifications to the designated Discord channel. The data is solely used for this purpose and is not shared with any third parties, except as described in this Policy.
+                </div>
+                <div>
+                    b. Communication: We may occasionally send you administrative messages, updates, or important notifications related to the bot service.
+                </div>   
+                <div>
+                    c. Improvements and Analytics: We may analyze the aggregated data to improve the functionality and performance of the bot. This may include identifying usage patterns, detecting and fixing technical issues, and enhancing user experience.
+                </div>   
+            </div>
+       </div> 
+
+       <div class="space-y-2">
+            <div class="text-3xl font-bold">3. Data Storage and Security</div>
+            <div class="space-y-2">
+                <div>
+                    a. Data Retention: We retain the collected data for the duration necessary to provide the bot service. Once the data is no longer required, we will securely delete or anonymize it.
+                </div>
+                <div>
+                    b. Data Security: We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or alteration. However, please note that no method of transmission over the internet or electronic storage is completely secure.
+                </div>   
+                <div>
+                    c. Data Sharing: We will not share or sell any user data collected by the bot with third parties unless required by law or with your explicit consent.
+                </div>   
+            </div>
+       </div> 
+
+        
+        <div class="space-y-2">
+            <div class="text-3xl font-bold">4. Children's Privacy</div>
+            <div class="space-y-2">
+                Our bot is intended for use by individuals who are at least 13 years old. We do not knowingly collect personal information from children under the age of 13. If we become aware that we have inadvertently collected personal data from a child under 13, we will promptly delete it from our records.
+            </div>
+        </div>
+
+        <div class="space-y-2">
+            <div class="text-3xl font-bold">5. Changes to the Privacy Policy</div>
+            <div class="space-y-2">
+                We reserve the right to modify or update this Privacy Policy at any time. Any changes will be effective upon posting the revised Policy on our website. We encourage you to review this Policy periodically for any updates.
+            </div>
+        </div>
+
+        <div class="space-y-2">
+            <div class="text-3xl font-bold">6. Contact Information</div>
+            <div class="space-y-2">
+                If you have any questions, concerns, or feedback regarding this Privacy Policy or our Discord bot, please contact us at contact@rappytv.com
+            </div>
+        </div>
+    </div>
+</div>
