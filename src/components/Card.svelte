420ae4f266e1b17ce33f1a2751a72e8c4e3af4a4
holybaechu <joonhoo0524@naver.com>
2024-02-24 10:01:16 +0900

    Responsive design

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,7 +2,7 @@
     export let title = "";
 </script>
 
-<div class="h-80 bg-[#ffffff04] rounded-lg outline outline-1 outline-[#cbd5e11a] p-6 space-y-2">
+<div class="h-[430px] lg:h-80 bg-[#ffffff04] rounded-lg outline outline-1 outline-[#cbd5e11a] p-6 space-y-2">
     <div class="text-2xl font-semibold">{title}</div>
     <div class="text-md text-gray-400">
         <slot/>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
-<div class="flex h-56 bg-[#00000025] justify-between">
-    <div class="flex flex-col p-10 h-full space-y-16 text-gray-400">
+<div class="flex bg-[#00000025] flex-col sm:flex-row justify-between">
+    <div class="flex flex-col p-10 space-y-2 text-gray-400">
         <a target="_blank" href="/contact">
             <div class="text-2xl font-semibold">RappyTV</div>
             <div class="text-md">© {new Date().getFullYear()} RappyTV</div>
@@ -9,7 +9,7 @@
     </div>
 
     <!-- Links -->
-    <div class="flex p-10 text-gray-400 text-right space-x-6">
+    <div class="flex p-10 text-gray-400 sm:text-right space-x-4">
         <div class="flex flex-col space-y-4">
             <a href="/docs/privacy">
                 Privacy Policy
