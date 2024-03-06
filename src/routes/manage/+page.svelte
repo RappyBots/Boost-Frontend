9dad8efaf0c902772440dcb4cadf0c189855cb86
holybaechu <joonhoo0524@naver.com>
2024-03-06 22:03:59 +0900

    Add gap between cards

--- src/routes/manage/+page.svelte
+++ src/routes/manage/+page.svelte
@@ -9,7 +9,16 @@
 <div class="flex flex-col justify-center items-center p-16 space-y-20">
     <h1 class="text-4xl font-bold">Manage your servers</h1>
 
-	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-5xl w-full justify-center">
+	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-5xl w-full justify-center gap-4">
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
+		<ServerCard/>
 		<ServerCard/>
 	</div>
 </div>
