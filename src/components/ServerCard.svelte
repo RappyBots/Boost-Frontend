8a7814781337a93d236ec79300fff433fa370e40
holybaechu <joonhoo0524@naver.com>
2024-03-06 22:06:17 +0900

    Improved card design

--- src/components/ServerCard.svelte
+++ src/components/ServerCard.svelte
@@ -6,13 +6,13 @@
 
 <div class="flex flex-col h-80 w-full rounded-lg outline outline-1 outline-[#cbd5e11a] bg-contain overflow-hidden" id="card">
     <div class="h-[125px] w-full bg-[#161c29] mt-auto">
-        <div class="flex mx-4 space-x-2">
+        <div class="flex mx-4 space-x-3">
             <img
                 class="-my-8 max-w-[80px] max-h-[80px] rounded-full"
                 src="https://cdn.discordapp.com/icons/260471731809026048/a_25d613b5a716ab94516c6217e7d3248a.webp?size=96" alt="Icon of LabyMod"
             />
 
-            <div class="text-[30px] font-bold">
+            <div class="my-1 text-[28px] font-bold">
                 LabyMod
             </div>
         </div>
