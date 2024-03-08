f54dfe480b555d70ee04d3e7c7d116b44346dae1
holybaechu <joonhoo0524@naver.com>
2024-03-08 16:48:12 +0900

    Delete `config.ts`

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Change the configs if you want ;)
-// You cannot use dotenv nor environment variables in this file as it doesn't work on client-side
-
-export const discord = {
-    clientID: "1067880912538304583",
-}
