8c334e52271d2447f5e76f931deab5b2031c3e67
holybaechu <holyb@holyb.xyz>
2024-03-26 21:36:44 +0900

    Add onInput

--- src/components/TextInput.svelte
+++ src/components/TextInput.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
     export let placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-    export let value = "";
+    export let value = ""; // replace with writeable store
+    export let onInput = (a: any) => {};
 </script>
 
-<input type="text" class="w-1/2 h-full p-2 bg-[#4949491a] rounded-lg outline-none" placeholder={placeholder} value={value}/>
+<input type="text" class="w-1/2 h-full p-2 bg-[#4949491a] rounded-lg outline-none" placeholder={placeholder} value={value} on:input={onInput}/>
